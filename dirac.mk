ff1e2d1950932f801f1d4ee031c97de01d520905
jhenrique09 <jhenrique09.mcz@hotmail.com>
2019-02-04 13:33:51 +0000

    whyred: Add Dirac Blobs
    Change-Id: I2106e1c59cd6f5e33d0676236a2c6805b265935f

---
+++ dirac.mk
@@ -0,0 +1,5 @@
+
+PRODUCT_COPY_FILES += \
+    vendor/xiaomi/Dirac/proprietary/vendor/etc/diracmobile.config:$(TARGET_COPY_OUT_VENDOR)/etc/diracmobile.config \
+    vendor/xiaomi/Dirac/proprietary/vendor/lib/libDiracAPI_SHARED.so:$(TARGET_COPY_OUT_VENDOR)/lib/libDiracAPI_SHARED.so \
+    vendor/xiaomi/Dirac/proprietary/vendor/lib/soundfx/libdirac.so:$(TARGET_COPY_OUT_VENDOR)/lib/soundfx/libdirac.so
